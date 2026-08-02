4c45821b8c6d2adbded7b34eb8f602b3aab4ba5e
ENOCK2008 <107791052+ENOCK2008@users.noreply.github.com>
2026-08-02 03:13:53 -0700

    1
    Signed-off-by: ENOCK2008 <107791052+ENOCK2008@users.noreply.github.com>

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,10 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
+from .models import Product
+
+
 @login_required
 def add_to_cart(request, product_id):
     """Add a product to the user's cart."""
@@ -5,26 +12,24 @@ def add_to_cart(request, product_id):
         product = get_object_or_404(Product, id=product_id)
     except:
         messages.error(request, f"Product with ID {product_id} does not exist. Please check the product ID.")
-        return redirect('shop:shop')  # Redirect to shop page
+        return redirect('shop:shop')
 
     if product.stock <= 0:
         messages.warning(request, "Sorry, this product is out of stock.")
         return redirect('shop:product_detail', product_id=product.id)
 
-    # Retrieve the cart from the session
     cart = request.session.get('cart', {})
 
-    # Update cart
-    if product_id in cart:
-        cart[product_id]['quantity'] += 1
+    if str(product_id) in cart:
+        cart[str(product_id)]['quantity'] += 1
     else:
-        cart[product_id] = {
+        cart[str(product_id)] = {
             'name': product.name,
             'price': str(product.price),
             'quantity': 1,
         }
 
-    request.session['cart'] = cart  # Save the updated cart to the session
+    request.session['cart'] = cart
 
     messages.success(request, f"{product.name} has been added to your cart.")
     return redirect('shop:cart_view')
